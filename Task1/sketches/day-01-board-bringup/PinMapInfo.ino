ff2b99b8b7dfee4a60f27eddba676599adc7bb49
anamika2005 <anamikass146@gmail.com>
2026-06-04 06:22:57 +0530

    Add setup function for serial communication
    Initialize serial communication and print pin mapping information.

--- Task1/sketches/day-01-board-bringup/PinMapInfo.ino
+++ Task1/sketches/day-01-board-bringup/PinMapInfo.ino
@@ -1,1 +1,9 @@
+void setup() {
+  Serial.begin(9600);
 
+  Serial.println(F("Analog pins A0-A5 map to digital 14-19"));
+  Serial.println(F("Avoid pins 0/1 when using USB Serial"));
+}
+
+void loop() {
+}
